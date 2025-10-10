e69d547d59c3e9093a0ec84f5cd6f3479c7ff3a8
Kayzendrew <140522846+Kayzendrew@users.noreply.github.com>
2025-10-10 10:34:57 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+const botoes = document.querySelectorAll(".botao");
+cont textos = document.querySelectorAll("aba.-conteudo);
+for(let i = 0; i < botoes.length; i++) {
+  botoes[i]
+}                                        
