70b8b35d874350cfee6dee1ac02baa991d227d51
Kayzendrew <140522846+Kayzendrew@users.noreply.github.com>
2025-11-06 14:01:49 -0300

    Refactor cronometro update logic and return values

--- main.js
+++ main.js
@@ -28,17 +28,28 @@ contadores[0].textContent = calculaTempo(tempos[0]);
 
 function atualizaCronometro(){
 for (let i=0; i<contadores.length;i++){
-contadores[i].textContent = calculaTempo(tempos[i]); } }
-function comecaCronometro(){
-atualizaCronometro();
-setInterval(atualizaCronometro,1000); }
-comecaCronometro();}
+document.getElementById(“dias”+i).textContent =
+
+calculaTempo(tempos[i])[0];
+
+document.getElementById(“horas”+i).textContent =
+
+calculaTempo(tempos[i])[1];
+
+document.getElementById(“min”+i).textContent =
+
+calculaTempo(tempos[i])[2];
+
+document.getElementById(“seg”+i).textContent =
+
+calculaTempo(tempos[i])[3];
+console.log(“seg”+i);
+
 }
 }
 
 if (tempoFinal > 0){
-return dias + “ dias “ + horas + “ horas “ + minutos + “ 
-minutos “ + segundos + “ segundos”;
+return [dias,horas,minutos,segundos]
 } else {
-return “Prazo Finalizado”;
+return [0,0,0,0];
   }
